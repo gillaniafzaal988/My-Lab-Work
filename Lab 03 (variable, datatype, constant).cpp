d8687cd891fd8ee1d30f20b849319aa2da98762a
gillaniafzaal988 <gillaniafzaal988@gmail.com>
2025-03-15 22:56:03 +0500

    Add files via upload

---
+++ Lab 03 (variable, datatype, constant).cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+/* {
+
+ double x1,x2,y1,y2,squared_distance;
+ x1=3;
+ x2=6;
+ y1=4;
+ y2=8;
+ squared_distance=(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+ cout<<"The squared distance between the points "<<"("<<x1<<","<<y1<<")"<<" and "
+     <<"("<<x2<<","<<y2<<")"<<" is: "<<squared_distance<<endl;
+ return 0;
+}
+
+*/
+//Program2
+/*
+{
+double item1,item2,item3,item4,item5,subtotal,tax,total;
+item1=12.95;
+ item2=24.95;
+ item3=6.95;
+ item4=14.95;
+ item5=3.95;
+
+ subtotal=item1+item2+item3+item4+item5;
+ const double tax_rate=0.06; tax=subtotal*tax_rate;
+ total=subtotal+tax;
+
+ cout<<"Price of item 1: $"<<item1<<endl;
+ cout<<"Price of item 2: $"<<item2<<endl;
+ cout<<"Price of item 3: $"<<item3<<endl;
+ cout<<"Price of item 4: $"<<item4<<endl;
+ cout<<"Price of item 5: $"<<item5<<endl;
+ cout<<"Subtotal: $"<<subtotal<<endl;
+ cout<<"Sales Tax (6%): $"<<tax<<endl;
+ cout<<"Total: $"<<total<<endl;
+ return 0;
+}
+*/
+ //*****************************************(Trip Cost Estimator)***************************//
+/*
+{
+ double distance,effiency,fuelCost,tripcost;
+ cout<<"Trip Cost Estimator"<<endl;
+ cout<<"*******************"<<endl;
+ cout<<"Enter distance of the trip in kilometers: ";
+ cin>>distance;
+ cout<<"Enter vehicle Fuel Efficiency(km per litre): ";
+ cin>>effiency;
+ cout<<"Enter Fuel cost per litre: ";
+ cin>>fuelCost;
+ tripcost=(distance/effiency)*fuelCost;
+ cout<< "Total trip cost: Rs "<<tripcost<<endl;
+
+ return 0;
+}*/
+
+
+ //********************************************BMI Calculator **********************************//
+/*
+{
+ double weight,height,BMI;
+ cout<<"BMI Calculator"<<endl;
+ cout<<"**************"<<endl;
+ cout<<"Enter your weight in Kilograms:";
+ cin>>weight;
+ cout<<"Enter your height in meters: ";
+ cin>>height;
+ BMI=weight/(height*height);
+ cout<<"Your BMI is: "<<BMI<<endl;
+ return 0;
+}
+*/
+
+
+
+
+ //********************************************** Arithmetic Operators****************************//
+/*
+ {
+ 
+ int no1,no2,sum,sub,multiply,division,mod;
+ cout<<"Enter two numbers :"<<endl;
+ cout<<"Number 1 :";
+ cin>>no1;
+ cout<<"Number 2 :";
+ cin>>no2;
+ sum=no1+no2;
+ sub=no1-no2;
+ multiply=no1*no2;
+ division=no1/no2;
+ mod=no1%no2;
+ cout<<"Addition:"<<endl;
+ cout<<"\t"<<no1<<" + "<<no2<<" = "<<sum<<endl;
+ cout<<"Subtraction:"<<endl;
+ cout<<"\t"<<no1<<" - "<<no2<<" = "<<sub<<endl;
+ cout<<"Multiplication:"<<endl;
+ cout<<"\t"<<no1<<" * "<<no2<<" = "<<multiply<<endl;
+ cout<<"Division:"<<endl;
+ cout<<"\t"<<no1<<" / "<<no2<<" = "<<division<<endl;
+ cout<<"Modulus:"<<endl;
+ cout<<"\t"<<no1<<" % "<<no2<<" = "<<mod<<endl;
+ return 0;
+}
+
+*/
+
+
+
+
+
+//********************************Average of three Numbers ********************************//
+/*
+{
+ double number1,number2,number3,sum,average;
+ number1=85.5;
+ number2=90.75;
+ number3=88.25;
+ sum=number1+number2+number3;
+ average=sum/3;
+ cout<<fixed<<setprecision(2)<<endl;
+ cout<<"The average of "<<number1<<", "<<number2<<" and "<<number3<<" = "<<average<<endl;
+ return 0;
+
+}*/
+
+
