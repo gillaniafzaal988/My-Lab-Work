dcbd3cd6cb1a997b4b558098f8155b1cc10b7fd3
gillaniafzaal988 <gillaniafzaal988@gmail.com>
2025-03-15 23:12:19 +0500

    Add files via upload

---
+++ Lab 05 some programs.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+//***************************Positive Number***************************//
+
+{
+    int num;
+    cout<<"Enter an integer:";
+    cin>>num;
+    if(num>0)
+    cout<<"The number is positive.";
+    return 0;
+}
+
+
+//***************************Even Number***************************//
+/*
+{
+    int num;
+    cout<<"Enter an integer:";
+    cin>>num;
+    if(num%2==0)
+    cout<<"The number is even.";
+    return 0;
+}
+*/
+
+//***************************Largest of Three Numbers***************************//
+/*
+{
+    int a,b,c,largest;
+    cout<<"Enter three numbers:";
+    cin>>a>>b>>c;
+    largest=a;
+    if(b>largest)
+    largest=b;
+    if(c>largest)
+    largest=c;
+    cout<<"The largest number is:"<<largest;
+    return 0;
+}
+*/
+
+//***************************Student Grade***************************//
+/*
+{
+    int marks;
+    cout<<"Enter marks out of 100:";
+    cin>>marks;
+    if(marks>=90)
+    cout<<"Grade:A+";
+    if(marks>=80 && marks<90)
+    cout<<"Grade:A";
+    if(marks>=70 && marks<80)
+    cout<<"Grade:B";
+    if(marks>=60 && marks<70)
+    cout<<"Grade:C";
+    if(marks<60)
+    cout<<"Fail";
+    return 0;
+}
+*/
+
+//***************************Traffic Signal System***************************//
+/*
+{
+    char signal;
+    cout<<"Enter traffic signal color (R/G/Y):";
+    cin>>signal;
+    if(signal=='R'||signal=='r')
+    cout<<"Stop";
+    if(signal=='G'||signal=='g')
+    cout<<"Go";
+    if(signal=='Y'||signal=='y')
+    cout<<"Slow Down";
+    return 0;
+}
+*/
+
+
+
